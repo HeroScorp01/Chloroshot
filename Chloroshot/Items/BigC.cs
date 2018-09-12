44a9a749d2c4b490c873f96d23ec935a14480545
HeroScorp01 <42976479+HeroScorp01@users.noreply.github.com>
2018-09-12 11:07:53 -0500

    Need to add Chloroshot.Projectiles

--- Chloroshot/Items/BigC.cs
+++ Chloroshot/Items/BigC.cs
@@ -1,1 +1,43 @@
+using Chloroshot.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace Chloroshot.Items
+{
+	public class BigC : ModItem
+	{
+		public override void SetDefaults()
+		{
+			// Alter any of these values as you see fit, but you should probably keep useStyle on 1, as well as the noUseGraphic and noMelee bools
+			item.shootSpeed = 10f;
+			item.damage = 100;
+			item.knockBack = 5f;
+			item.useStyle = 1;
+			item.useAnimation = 25;
+			item.useTime = 25;
+			item.width = 30;
+			item.height = 30;
+			item.maxStack = 99;
+			item.rare = 7;
+
+			item.consumable = true;
+			item.noUseGraphic = true;
+			item.noMelee = true;
+			item.autoReuse = true;
+			item.thrown = true;
+
+			item.UseSound = SoundID.Item1;
+			item.value = Item.sellPrice(silver: 5);
+			item.shoot = mod.ProjectileType<ExampleJavelinProjectile>();
+		}
+    public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			tile.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
